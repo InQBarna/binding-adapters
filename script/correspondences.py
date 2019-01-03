6b15c0efb76adf2fe4f53f7ed53a259c89593c41
David García <david.garcia@inqbarna.com>
2019-01-03 10:18:32 +0100

    Starting migration script

---
+++ script/correspondences.py
@@ -0,0 +1,30 @@
+import urllib.request
+import csv
+import io
+from logger import *
+
+
+class Correspondences:
+    def __init__(self, url):
+        self.url = url
+        self._mapping = {}
+        self._do_create_correspondences()
+
+    def _do_create_correspondences(self):
+        with urllib.request.urlopen(self.url) as response:
+            with io.TextIOWrapper(response) as content:
+                mapping = csv.DictReader(content)
+                fromKey = None
+                toKey = None
+                for row in mapping:
+                    if not fromKey or not toKey:
+                        fromKey = mapping.fieldnames[0]
+                        toKey = mapping.fieldnames[1]
+                        pdbg("FromKey = %s" % fromKey)
+                        pdbg("ToKey = %s" % toKey)
+                    pdbg("rowFrom = %s" % row[fromKey])
+                    self._mapping[row[fromKey]] = row[toKey]
+
+        for f,t in self._mapping.items():
+            pdbg("From '%s' -> '%s'" % (f, t))
+
